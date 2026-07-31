9e68ea02e14b6912eef119a315778d4576b420d6
Leonardo <leona@example.com>
2026-07-31 14:58:59 -0300

    fix: configurar Netlify para build Vite SPA (dist + _redirects + React Router MobileBottomNav)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MobileBottomNav from '@/components/MobileBottomNav';
 import Home from '@/app/page';
 import BuscaPage from '@/app/busca/page';
 import CategoryPage from '@/app/categoria/[slug]/page';
@@ -40,47 +41,50 @@ import AdminAvaliacoesPage from '@/app/admin/avaliacoes/page';
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Main Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/busca" element={<BuscaPage />} />
-        <Route path="/categoria/:slug" element={<CategoryPage />} />
-        <Route path="/produto/:id" element={<ProductDetailPage />} />
-        <Route path="/carrinho" element={<CarrinhoPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/cadastro" element={<CadastroPage />} />
-        <Route path="/favoritos" element={<FavoritosPage />} />
-        <Route path="/pedidos" element={<PedidosPage />} />
-        <Route path="/pedidos/:id" element={<PedidoDetailPage />} />
-        <Route path="/pedido/:id/confirmacao" element={<ConfirmacaoPedidoPage />} />
-        <Route path="/sobre" element={<SobrePage />} />
-        <Route path="/contato" element={<ContatoPage />} />
+      <div className="flex-1 flex flex-col min-h-screen pb-mobile-nav md:pb-0">
+        <Routes>
+          {/* Main Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/busca" element={<BuscaPage />} />
+          <Route path="/categoria/:slug" element={<CategoryPage />} />
+          <Route path="/produto/:id" element={<ProductDetailPage />} />
+          <Route path="/carrinho" element={<CarrinhoPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/cadastro" element={<CadastroPage />} />
+          <Route path="/favoritos" element={<FavoritosPage />} />
+          <Route path="/pedidos" element={<PedidosPage />} />
+          <Route path="/pedidos/:id" element={<PedidoDetailPage />} />
+          <Route path="/pedido/:id/confirmacao" element={<ConfirmacaoPedidoPage />} />
+          <Route path="/sobre" element={<SobrePage />} />
+          <Route path="/contato" element={<ContatoPage />} />
 
-        {/* Minha Conta Nested Routes */}
-        <Route path="/minha-conta" element={<MinhaContaLayout><MinhaContaPage /></MinhaContaLayout>} />
-        <Route path="/minha-conta/perfil" element={<MinhaContaLayout><PerfilPage /></MinhaContaLayout>} />
-        <Route path="/minha-conta/enderecos" element={<MinhaContaLayout><EnderecosPage /></MinhaContaLayout>} />
-        <Route path="/minha-conta/pedidos" element={<MinhaContaLayout><MinhaContaPedidosPage /></MinhaContaLayout>} />
-        <Route path="/minha-conta/pedidos/:id" element={<MinhaContaLayout><MinhaContaPedidoDetailPage /></MinhaContaLayout>} />
+          {/* Minha Conta Nested Routes */}
+          <Route path="/minha-conta" element={<MinhaContaLayout><MinhaContaPage /></MinhaContaLayout>} />
+          <Route path="/minha-conta/perfil" element={<MinhaContaLayout><PerfilPage /></MinhaContaLayout>} />
+          <Route path="/minha-conta/enderecos" element={<MinhaContaLayout><EnderecosPage /></MinhaContaLayout>} />
+          <Route path="/minha-conta/pedidos" element={<MinhaContaLayout><MinhaContaPedidosPage /></MinhaContaLayout>} />
+          <Route path="/minha-conta/pedidos/:id" element={<MinhaContaLayout><MinhaContaPedidoDetailPage /></MinhaContaLayout>} />
 
-        {/* Admin Nested Routes */}
-        <Route path="/admin" element={<AdminLayout><AdminDashboardPage /></AdminLayout>} />
-        <Route path="/admin/relatorios" element={<AdminLayout><AdminRelatoriosPage /></AdminLayout>} />
-        <Route path="/admin/produtos" element={<AdminLayout><AdminProdutosPage /></AdminLayout>} />
-        <Route path="/admin/produtos/novo" element={<AdminLayout><AdminNovoProdutoPage /></AdminLayout>} />
-        <Route path="/admin/produtos/:id/editar" element={<AdminLayout><AdminEditarProdutoPage /></AdminLayout>} />
-        <Route path="/admin/categorias" element={<AdminLayout><AdminCategoriasPage /></AdminLayout>} />
-        <Route path="/admin/pedidos" element={<AdminLayout><AdminPedidosPage /></AdminLayout>} />
-        <Route path="/admin/pedidos/:id" element={<AdminLayout><AdminPedidoDetailPage /></AdminLayout>} />
-        <Route path="/admin/hero" element={<AdminLayout><AdminHeroPage /></AdminLayout>} />
-        <Route path="/admin/banners" element={<AdminLayout><AdminBannersPage /></AdminLayout>} />
-        <Route path="/admin/cupons" element={<AdminLayout><AdminCuponsPage /></AdminLayout>} />
-        <Route path="/admin/avaliacoes" element={<AdminLayout><AdminAvaliacoesPage /></AdminLayout>} />
+          {/* Admin Nested Routes */}
+          <Route path="/admin" element={<AdminLayout><AdminDashboardPage /></AdminLayout>} />
+          <Route path="/admin/relatorios" element={<AdminLayout><AdminRelatoriosPage /></AdminLayout>} />
+          <Route path="/admin/produtos" element={<AdminLayout><AdminProdutosPage /></AdminLayout>} />
+          <Route path="/admin/produtos/novo" element={<AdminLayout><AdminNovoProdutoPage /></AdminLayout>} />
+          <Route path="/admin/produtos/:id/editar" element={<AdminLayout><AdminEditarProdutoPage /></AdminLayout>} />
+          <Route path="/admin/categorias" element={<AdminLayout><AdminCategoriasPage /></AdminLayout>} />
+          <Route path="/admin/pedidos" element={<AdminLayout><AdminPedidosPage /></AdminLayout>} />
+          <Route path="/admin/pedidos/:id" element={<AdminLayout><AdminPedidoDetailPage /></AdminLayout>} />
+          <Route path="/admin/hero" element={<AdminLayout><AdminHeroPage /></AdminLayout>} />
+          <Route path="/admin/banners" element={<AdminLayout><AdminBannersPage /></AdminLayout>} />
+          <Route path="/admin/cupons" element={<AdminLayout><AdminCuponsPage /></AdminLayout>} />
+          <Route path="/admin/avaliacoes" element={<AdminLayout><AdminAvaliacoesPage /></AdminLayout>} />
 
-        {/* Fallback Catch-all Route */}
-        <Route path="*" element={<Home />} />
-      </Routes>
+          {/* Fallback Catch-all Route */}
+          <Route path="*" element={<Home />} />
+        </Routes>
+        <MobileBottomNav />
+      </div>
     </BrowserRouter>
   );
 }
